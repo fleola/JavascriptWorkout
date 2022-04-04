5f9b8ecd44ccf1fa47dba174a526dc419d5ff0ef
leo_feola <feola.leopoldo@gmail.com>
2022-04-04 11:09:02 +0200

    forOf

---
+++ Live/forOf.js
@@ -0,0 +1,11 @@
+const fruitsArray = ["banana", "apple", "mango"];
+
+for (let i = 0; i < fruitsArray.length; i++) {
+  console.log(fruitsArray[i]);
+}
+
+// for of
+
+for (let fruit of fruitsArray) {
+  console.log(fruit);
+}
